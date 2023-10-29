dce624ab41da8bda4c50ee639e6063752a979635
Trung <109146090+trungtddevweb@users.noreply.github.com>
2023-10-30 00:28:42 +0700

    cors

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ const app = express()
 
 // Middlewawre
 app.use(express.json())
-app.use(cors())
+app.use(
+    cors({
+        origin: ['*'],
+    })
+)
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }))
 app.use(morgan('combined'))
