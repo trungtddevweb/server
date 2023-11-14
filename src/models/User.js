c47f5cc81175b5afdfd44176badb28afddcc874d
Trung <109146090+trungtddevweb@users.noreply.github.com>
2023-11-14 18:21:58 +0700

    upp

--- src/models/User.js
+++ src/models/User.js
@@ -28,7 +28,6 @@ const UserSchema = new Schema(
         info: {
             phoneNumber: {
                 type: String,
-                unique: true,
             },
             address: {
                 type: String,
